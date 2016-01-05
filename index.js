864a4fc607a7dcf5b27b1df1bcf656a92ff1b63b
Christophe Porteneuve <tdd@tddsworld.com>
2016-01-05 16:03:09 +0100

    Add startLower flag

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 
-module.exports = function hackerCase(text) {
-  var upperCase = false;
+module.exports = function hackerCase(text, startLower) {
+  if (undefined === startLower) {
+    startLower = false;
+  }
+  var upperCase = startLower;
 
   return text.replace(/\w/g, function(char) {
     return char[(upperCase = !upperCase) ? 'toLocaleUpperCase' : 'toLocaleLowerCase']();
